3704894ec8bcfc42eafdc6511ac8e9ea0b7be1f3
Igor2398 <igor.slusarski1123@gmail.com>
2023-04-13 20:29:15 +0200

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Igor Ślusarski on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
